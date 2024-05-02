d21f62eeceed2306fec1b286707bafc9cb9270c4
Ali Gel <ali@gel.com>
2024-05-02 14:16:13 +0300

    initial

---
+++ src/test/java/pages/ContactListLogInPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class ContactListLogInPage {
+
+    public ContactListLogInPage (){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "signup")
+    public WebElement signupButton;
+
+
+
+
+
+
+
+
+}
